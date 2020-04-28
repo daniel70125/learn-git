c07ed856eedc1df169a4f3ed738e380be27ce7f3
daniel70125 <dwrighttt504@gmail.com>
2020-04-28 10:50:44 -0500

    Hello

---
+++ index.js
@@ -0,0 +1,114 @@
+/*
+
+
+Last login: Mon Apr 27 12:51:08 on ttys000
+Users-MBP-6:~ user$ cd documents/firstPJ
+Users-MBP-6:firstPJ user$ ls
+app.js			mariokart.png		scroll.html
+cloud1.png		node_modules		space.jpg
+cloud2.png		package-lock.json	spaceship.jpg
+express.js		package.json		tree.png
+index.html		react.html
+marioDesk.html		react2.html
+Users-MBP-6:firstPJ user$ pwd
+/Users/user/documents/firstPJ
+Users-MBP-6:firstPJ user$ touch href.js
+Users-MBP-6:firstPJ user$ ls
+app.js			marioDesk.html		react2.html
+cloud1.png		mariokart.png		scroll.html
+cloud2.png		node_modules		space.jpg
+express.js		package-lock.json	spaceship.jpg
+href.js			package.json		tree.png
+index.html		react.html
+Users-MBP-6:firstPJ user$ rm  href.js
+Users-MBP-6:firstPJ user$ ls
+app.js			mariokart.png		scroll.html
+cloud1.png		node_modules		space.jpg
+cloud2.png		package-lock.json	spaceship.jpg
+express.js		package.json		tree.png
+index.html		react.html
+marioDesk.html		react2.html
+Users-MBP-6:firstPJ user$ cd..
+-bash: cd..: command not found
+Users-MBP-6:firstPJ user$ mkdir wr-1
+Users-MBP-6:firstPJ user$ cd wr-1
+Users-MBP-6:wr-1 user$ mkdir DevMountain
+Users-MBP-6:wr-1 user$ rm wr-1
+rm: wr-1: No such file or directory
+Users-MBP-6:wr-1 user$ ls
+DevMountain
+Users-MBP-6:wr-1 user$ cd..
+-bash: cd..: command not found
+Users-MBP-6:wr-1 user$ cd ..
+Users-MBP-6:firstPJ user$ 
+Users-MBP-6:firstPJ user$ rm wr-1
+rm: wr-1: is a directory
+Users-MBP-6:firstPJ user$ ls
+app.js			mariokart.png		scroll.html
+cloud1.png		node_modules		space.jpg
+cloud2.png		package-lock.json	spaceship.jpg
+express.js		package.json		tree.png
+index.html		react.html		wr-1
+marioDesk.html		react2.html
+Users-MBP-6:firstPJ user$ cd wr-1
+Users-MBP-6:wr-1 user$ rm DevMountain
+rm: DevMountain: is a directory
+Users-MBP-6:wr-1 user$ ls
+DevMountain
+Users-MBP-6:wr-1 user$ rm DevMountain
+rm: DevMountain: is a directory
+Users-MBP-6:wr-1 user$ cd ..
+Users-MBP-6:firstPJ user$ rm -rf wr-1
+Users-MBP-6:firstPJ user$ ls
+app.js			mariokart.png		scroll.html
+cloud1.png		node_modules		space.jpg
+cloud2.png		package-lock.json	spaceship.jpg
+express.js		package.json		tree.png
+index.html		react.html
+marioDesk.html		react2.html
+Users-MBP-6:firstPJ user$ mk dir DevMountain
+-bash: mk: command not found
+Users-MBP-6:firstPJ user$ mkdir DevMountain
+Users-MBP-6:firstPJ user$ cd  DevMountain
+Users-MBP-6:DevMountain user$ ls
+Users-MBP-6:DevMountain user$ mkdir week-1 week-2 week-3 week-4 week-5 week-6 week-7
+Users-MBP-6:DevMountain user$ ls
+week-1	week-2	week-3	week-4	week-5	week-6	week-7
+Users-MBP-6:DevMountain user$ cd week-1
+Users-MBP-6:week-1 user$ pwd
+/Users/user/documents/firstPJ/DevMountain/week-1
+Users-MBP-6:week-1 user$ mkdir day-1 day-2 day-3 day-4 day-5
+Users-MBP-6:week-1 user$ ls
+day-1	day-2	day-3	day-4	day-5
+Users-MBP-6:week-1 user$ cd day-1
+Users-MBP-6:day-1 user$ clear
+
+Users-MBP-6:day-1 user$ touch.txt
+-bash: touch.txt: command not found
+Users-MBP-6:day-1 user$ touch test.txt
+Users-MBP-6:day-1 user$ code test.txt
+Users-MBP-6:day-1 user$ rm test.txt
+Users-MBP-6:day-1 user$ ls
+Users-MBP-6:day-1 user$ cd ..
+Users-MBP-6:week-1 user$ cd .
+Users-MBP-6:week-1 user$ cd ..
+Users-MBP-6:DevMountain user$ git config --global user.name"daniel70125"
+Users-MBP-6:DevMountain user$ git config --global user.email "dwrighttt504@gmail.com"
+Users-MBP-6:DevMountain user$ cd week-1
+Users-MBP-6:week-1 user$ cd day-1
+Users-MBP-6:day-1 user$ git clone https://github.com/daniel70125/learn-git.git
+Cloning into 'learn-git'...
+remote: Enumerating objects: 790, done.
+remote: Total 790 (delta 0), reused 0 (delta 0), pack-reused 790
+Receiving objects: 100% (790/790), 108.94 KiB | 987.00 KiB/s, done.
+Resolving deltas: 100% (279/279), done.
+Users-MBP-6:day-1 user$ ls
+learn-git
+Users-MBP-6:day-1 user$ cd learn-git/
+Users-MBP-6:learn-git user$ code .
+Users-MBP-6:learn-git user$ 
+
+
+
+
+*/
